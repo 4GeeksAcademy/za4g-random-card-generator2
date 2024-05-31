da627ed3a37087d3e4d9c38dd0ac00638d8f4473
za4g <138465437+za4g@users.noreply.github.com>
2024-05-31 19:55:55 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -1,33 +1,47 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("Generating random card...");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+let suitsShape = ["Heart", "Spade", "Flower", "Diamond"];
+let suitColor = ["red", "black"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomRank = Math.round(Math.random() * 12);
+let randomSuitShape = Math.round(Math.random() * 3);
+let randomSuitColor = Math.round(Math.random() * 1);
 
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+let randomSuitShapeIconTop = '';
+let randomSuitShapeIconBottom = '';
 
-  let generatedExcuse =
-    who[Math.round(Math.random() * 3)] +
-    " " +
-    action[Math.round(Math.random() * 3)] +
-    " " +
-    what[Math.round(Math.random() * 2)] +
-    " " +
-    when[Math.round(Math.random() * 4)];
+document.getElementById('suitTopLeft').style.color = suitColor[randomSuitColor];
 
-  document.getElementById("excuse").innerHTML = generatedExcuse;
-};
+/* suitShape[randomSuitShape]; */
+
+console.log("randomSuitShape", randomSuitShape)
+
+switch (suitsShape[randomSuitShape]) {
+  case 'Heart':
+		randomSuitShapeIconTop = '<i class="fa-solid fa-heart"></i>';
+		randomSuitShapeIconBottom = '<i class="fa-solid fa-heart fa-flip-vertical"></i>';
+    break;
+  case 'Spade':
+		randomSuitShapeIconTop = '<i class="fa-solid fa-spade"></i>'
+		randomSuitShapeIconBottom = '<i class="fa-solid fa-spade fa-flip-vertical"></i>'
+    break;    
+  case 'Flower':
+		randomSuitShapeIconTop = '<i class="fa-solid fa-club"></i>'
+		randomSuitShapeIconBottom = '<i class="fa-solid fa-club fa-flip-vertical"></i>'
+    break;
+  case 'Diamond':
+		randomSuitShapeIconTop = '<i class="fa-solid fa-diamond"></i>';
+		randomSuitShapeIconBottom = '<i class="fa-solid fa-diamond fa-flip-vertical"></i>';
+    break;
+}
+
+console.log("randomSuitShapeIconTop", randomSuitShapeIconTop);
+console.log("randomSuitShapeIconBottom", randomSuitShapeIconBottom)
+
+document.getElementById('suitTopLeft').innerHTML = randomSuitShapeIconTop;
+
+document.getElementById('rank').innerHTML = ranks[randomRank];
+
+document.getElementById('suitBottomRight').style.color = suitColor[randomSuitColor];
+document.getElementById('suitBottomRight').innerHTML = randomSuitShapeIconBottom;
